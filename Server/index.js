ac106f0a8b637e810930d0bbdeeafe3df60e8e8a
MCBOOK <mcbook@MCBOOKs-MacBook-Air.local>
2024-08-26 00:20:02 +0000

    tee

--- Server/index.js
+++ Server/index.js
@@ -79,11 +79,14 @@ app.post("/addUser", mongoConnection, async (req, res) => {
   try {
     // Create a new user
     const newUser = await UserModel.create({ name, Index_No });
-    if (newUser) {
-      return res.status(201).json(newUser);
-    }
+    // if (newUser) {
+    // return res.status(201).json(newUser);
+    // }
+    return res.status(201).json(newUser);
   } catch (error) {
-    res.status(500).json({ message: "There was an error saving the user" });
+    return res
+      .status(500)
+      .json({ message: "There was an error saving the user" });
   }
 });
 
